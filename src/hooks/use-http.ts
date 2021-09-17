96353438f33c98a2af596b115e968d5d3429e247
Marcin Wilk <marolis1239@gmail.com>
2021-09-17 23:43:49 +0200

    craete useHttp hook

---
+++ src/hooks/use-http.ts
@@ -0,0 +1,66 @@
+import { useCallback, useState } from "react";
+
+const DB_URL = "http://localhost:8080";
+
+interface UseHttpHook {
+    isLoading: boolean;
+    error: string;
+    sendRequest: (url: string, method?: string, body?: any) => Promise<any>;
+}
+
+const useHttp = (): UseHttpHook => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
+
+    const sendRequest = useCallback(
+        async (url: string, method?: string, body?: any) => {
+            setIsLoading(true);
+            setError("");
+
+            let headers: HeadersInit = {};
+
+            if (method !== "GET") {
+                headers["Content-Type"] = "application/json";
+            }
+
+            const requestInit: RequestInit = {
+                method,
+                headers,
+                body: JSON.stringify(body),
+            };
+
+            try {
+                const response = await fetch(DB_URL + url, requestInit);
+
+                const data = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(data.message);
+                }
+
+                setIsLoading(false);
+
+                return data;
+            } catch (err) {
+                let errorMessage = "Something went wrong!";
+
+                if ((err as Error).message) {
+                    errorMessage = (err as Error).message; // ?????
+                }
+
+                setError(errorMessage);
+            }
+
+            setIsLoading(false);
+        },
+        []
+    );
+
+    return {
+        isLoading,
+        error,
+        sendRequest,
+    };
+};
+
+export default useHttp;
