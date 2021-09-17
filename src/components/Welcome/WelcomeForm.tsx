de93b8bfaa1e9d18d8544e90780183d31129ae44
Marcin Wilk <marolis1239@gmail.com>
2021-09-17 19:12:00 +0200

    fix welcome page layout

--- src/components/Welcome/WelcomeForm.tsx
+++ src/components/Welcome/WelcomeForm.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
 
+import classes from "./styles/WelcomeForm.module.css";
+
 const WelcomeForm: React.FC = () => {
     return (
-        <form>
+        <form className={classes.welcomeForm}>
             <Input placeholder="Z"/>
             <Input placeholder="DO"/>
             <Input type="date" placeholder="Z"/>
-            <Button type="submit"><b>ZNAJDZ</b> POLACZENIE</Button>
+            <Button type="submit"><b>ZNAJDŹ</b> POŁĄCZENIE</Button>
         </form>
     );
 };
