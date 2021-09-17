8204c89b9498e041258665d099299b4db8e7e52a
Marcin Wilk <marolis1239@gmail.com>
2021-09-17 19:53:48 +0200

    create register form

--- src/components/Auth/Signup.tsx
+++ src/components/Auth/Signup.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../UI/Button/Button';
+import Input from '../UI/Input/Input';
+
+import classes from "./styles/Signup.module.css";
 
 const Signup: React.FC = () => {
     return (
-        <div>Signup</div>
+        <div className={classes.signup}>
+            <form>
+                <Input type="email" placeholder="Adres e-mail"/>
+                <Input type="password" placeholder="Hasło (co najmniej 8 znaków)"/>
+                <Input type="password" placeholder="Potwierdzenie hasła (co najmniej 8 znaków)"/>
+                <Button type="submit">Zarejestruj się</Button>
+            </form>
+            <p>Masz juz konto? <Link to="/signin">Zaloguj się</Link></p>
+        </div>
     );
 }
 
--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -4,10 +4,17 @@ import classes from "./styles/Button.module.css";
 interface Props {
     className?: string;
     type?: "button" | "submit" | "reset";
+    disabled?: boolean;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<Props> = ({ children, className, type, onClick }) => {
+const Button: React.FC<Props> = ({
+    children,
+    className,
+    type,
+    disabled,
+    onClick,
+}) => {
     let compiledClassName = classes.button;
 
     if (className) {
@@ -15,7 +22,12 @@ const Button: React.FC<Props> = ({ children, className, type, onClick }) => {
     }
 
     return (
-        <button className={compiledClassName} type={type} onClick={onClick}>
+        <button
+            className={compiledClassName}
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+        >
             {children}
         </button>
     );
