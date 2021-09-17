2228d9dacbd9775c68afc7a81ae984db7d903716
Marcin Wilk <marolis1239@gmail.com>
2021-09-17 20:05:36 +0200

    create signin form

--- src/components/Auth/Signin.tsx
+++ src/components/Auth/Signin.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+
+import classes from "./styles/Signin.module.css";
 
 const Signin: React.FC = () => {
     return (
-        <div>Signin</div>
+        <div className={classes.signin}>
+            <form>
+                <Input type="email" placeholder="Adres e-mail" />
+                <Input type="password" placeholder="Hasło" />
+                <Button type="submit">Zarejestruj się</Button>
+            </form>
+            <div className={classes.bottom}>
+                <Link to="/signup">Zarejestruj się</Link>
+                <Link to="/password/send">Zapomniałeś hasła?</Link>
+            </div>
+        </div>
     );
-}
+};
 
 export default Signin;
