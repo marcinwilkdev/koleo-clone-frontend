15590b324eac83cf8b73a731416fb95ec2ebf553
Marcin Wilk <marolis1239@gmail.com>
2021-09-18 13:34:05 +0200

    refactor auth components

--- src/components/Auth/AuthComponent.tsx
+++ src/components/Auth/AuthComponent.tsx
@@ -1,41 +1,15 @@
 import React from "react";
-import { Redirect, Route, Switch } from "react-router";
-import { Link } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import SplitLayout from "../Layout/SplitLayout";
-import AuthNav from "./AuthNav";
-import Signin from "./Signin";
-import Signup from "./Signup";
-import classes from "./styles/Auth.module.css";
+import MainAuth from "./MainAuth";
+
 
 const AuthComponent: React.FC = () => {
     return (
         <SplitLayout>
-            <div>
-                <h3>
-                    <b>Dlaczego</b> warto założyć konto?
-                </h3>
-                <p>
-                    Dzięki KOLEO w jednym miejscu kupisz wszystkie bilety na
-                    wszystkie pociągi. Szybko, bezpiecznie i wygodnie.
-                </p>
-                <Link to="/">
-                    <button>{"< "}wróć</button>
-                </Link>
-            </div>
-            <div className={classes.form}>
-                <AuthNav />
-                <Switch>
-                    <Route path="/auth/signin" exact>
-                        <Signin />
-                    </Route>
-                    <Route path="/auth/signup" exact>
-                        <Signup />
-                    </Route>
-                    <Route path="/auth/">
-                        <Redirect to="/not-found" />
-                    </Route>
-                </Switch>
-            </div>
+            <Switch>
+                <Route path="/auth" component={MainAuth} />
+            </Switch>
         </SplitLayout>
     );
 };
---
+++ src/components/Auth/MainAuth.tsx
@@ -0,0 +1,42 @@
+import React, { Fragment } from "react";
+import { Link, Switch, Route, Redirect } from "react-router-dom";
+import AuthNav from "./AuthNav";
+import Signin from "./Signin";
+import Signup from "./Signup";
+
+import classes from "./styles/MainAuth.module.css";
+
+const MainAuth: React.FC = () => {
+    return (
+        <Fragment>
+            <div>
+                <h3>
+                    <b>Dlaczego</b> warto założyć konto?
+                </h3>
+                <p>
+                    Dzięki KOLEO w jednym miejscu kupisz wszystkie bilety na
+                    wszystkie pociągi. Szybko, bezpiecznie i wygodnie.
+                </p>
+                <Link to="/">
+                    <button>{"< "}wróć</button>
+                </Link>
+            </div>
+            <div className={classes.form}>
+                <AuthNav />
+                <Switch>
+                    <Route path="/auth/signin" exact>
+                        <Signin />
+                    </Route>
+                    <Route path="/auth/signup" exact>
+                        <Signup />
+                    </Route>
+                    <Route path="/auth/">
+                        <Redirect to="/not-found" />
+                    </Route>
+                </Switch>
+            </div>
+        </Fragment>
+    );
+};
+
+export default MainAuth;
