9894649fb7aeb3ec63131b4b2e10e3c5bdaea435
Marcin Wilk <marolis1239@gmail.com>
2021-09-20 20:15:14 +0200

    remove Form component from Signup

--- src/components/Auth/MainAuth/Signup.tsx
+++ src/components/Auth/MainAuth/Signup.tsx
@@ -1,52 +1,64 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
+import useHttp from "../../../hooks/use-http";
 import useInput from "../../../hooks/use-input";
-import Form from "../../UI/Form/Form";
-import { InputInterface } from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
+import Input from "../../UI/Input/Input";
 
 import classes from "./styles/Signup.module.css";
 
 const Signup: React.FC = () => {
     const history = useHistory();
+    const { sendRequest } = useHttp();
 
     const emailHook = useInput((value) => value.includes("@"));
     const passwordHook = useInput((value) => value.length >= 8);
     const confirmPasswordHook = useInput((value) => value.length >= 8);
 
-    const inputs: InputInterface[] = [
-        {
-            name: "email",
-            hook: emailHook,
-            type: "email",
-            placeholder: "Adres e-mail",
-        },
-        {
-            name: "password",
-            hook: passwordHook,
-            type: "password",
-            placeholder: "Hasło (co najmniej 8 znaków)",
-        },
-        {
-            name: "confirmPassword",
-            hook: confirmPasswordHook,
-            type: "password",
-            placeholder: "Potwierdzenie hasła (co najmniej 8 znaków)",
-        },
-    ];
+    const isFormValid =
+        emailHook.isValid &&
+        passwordHook.isValid &&
+        confirmPasswordHook.isValid;
 
-    const afterSubmitCallback = () => {
+    const submitHandler: React.FormEventHandler<HTMLFormElement> = async (
+        event
+    ) => {
+        event.preventDefault();
+
+        const data = await sendRequest("/auth/signup", "PUT", {
+            email: emailHook.value,
+            password: passwordHook.value,
+            confirmPassword: confirmPasswordHook.value,
+        });
+
         history.push("/auth/discount-choice");
     };
 
     return (
         <div className={classes.signup}>
-            <Form
-                inputs={inputs}
-                submitLabel="Zarejestruj się"
-                submitUrl="/auth/signup"
-                submitMethod="PUT"
-                afterSubmitCallback={afterSubmitCallback}
-            />
+            <form onSubmit={submitHandler}>
+                <Input
+                    name="email"
+                    type="email"
+                    hook={emailHook}
+                    placeholder="Adres e-mail"
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    hook={passwordHook}
+                    placeholder="Hasło (co najmniej 8 znaków)"
+                />
+                <Input
+                    name="confirmPassword"
+                    type="password"
+                    hook={confirmPasswordHook}
+                    placeholder="Potwierdzenie hasła (co najmniej 8 znaków)"
+                />
+                <Button type="submit" disabled={!isFormValid}>
+                    Zaloguj się
+                </Button>
+            </form>
             <p>
                 Masz juz konto?{" "}
                 <Link className={classes.bottomLink} to="/auth/signin">
