9e022126c8385baf7e9d9e4b378fe79d7ad5435f
Marcin Wilk <marolis1239@gmail.com>
2021-09-21 13:43:21 +0200

    add MyOrders page

---
+++ src/components/Profile/Orders/MyOrders.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from '../../UI/Title/Title';
+import classes from './styles/MyOrders.module.css';
+
+const MyOrders: React.FC = () => {
+    return (
+        <div>
+            <h1><Title title="Moje bilety"/></h1>
+        </div>
+    );
+}
+
+export default MyOrders;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router";
+import MyOrders from "../components/Profile/Orders/MyOrders";
 import ProfileNav from "../components/Profile/ProfileNav/ProfileNav";
 
 const Profile: React.FC = () => {
@@ -7,7 +8,7 @@ const Profile: React.FC = () => {
         <Fragment>
             <ProfileNav />
             <Switch>
-                <Route path="/profile/my-orders" exact/>
+                <Route path="/profile/my-orders" component={MyOrders} exact/>
                 <Route path="/profile/passengers" exact/>
                 <Route path="/profile/my-data" exact/>
                 <Route path="/profile/finances" exact/>
