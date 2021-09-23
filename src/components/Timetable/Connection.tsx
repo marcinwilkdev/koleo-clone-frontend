190886735f40305c617d6f9ad47b76553f8e8aac
Marcin Wilk <marolis1239@gmail.com>
2021-09-23 17:16:01 +0200

    refactor connection component

--- src/components/Timetable/Connection.tsx
+++ src/components/Timetable/Connection.tsx
@@ -15,30 +15,15 @@ const Connection: React.FC<ISavedConnection> = ({ cities, trainType }) => {
     const departureCity = cities[0];
     const arrivalCity = cities[cities.length - 1];
 
-    const departureDate = new Date(departureCity.date);
-    const arrivalDate = new Date(arrivalCity.date);
-
     const price = 5.99;
-
-    const departureTime = `${departureDate.getHours()}:${departureDate.getMinutes()}`;
-    const arrivalTime = `${arrivalDate.getHours()}:${arrivalDate.getMinutes()}`;
-
-    const travelTimeInMinutes =
-        (arrivalDate.getTime() - departureDate.getTime()) / 60000;
-
-    const travelTimeHours = Math.floor(travelTimeInMinutes / 60);
-    const travelTimeMinutes = travelTimeInMinutes - travelTimeHours * 60;
-
-    const travelTimeString = `${travelTimeHours}h${travelTimeMinutes}`;
 
     return (
         <div className={classes.connection}>
             <ConnectionInfo
-                arrivalTime={arrivalTime}
-                departureTime={departureTime}
+                arrivalCity={arrivalCity}
+                departureCity={departureCity}
                 price={price}
                 trainType={trainType}
-                travelTimeString={travelTimeString}
                 opened={opened}
                 onClick={toggleOpened}
             />
--- src/components/Timetable/ConnectionInfo.tsx
+++ src/components/Timetable/ConnectionInfo.tsx
@@ -1,28 +1,55 @@
 import React from "react";
+import { ISavedCity } from "../../models/city";
 import classes from "./styles/ConnectionInfo.module.css";
 
+interface CityWithDate {
+    city: ISavedCity;
+    date: Date;
+}
+
 interface Props {
-    departureTime: string;
-    arrivalTime: string;
+    departureCity: CityWithDate;
+    arrivalCity: CityWithDate;
     trainType: string;
-    travelTimeString: string;
     price: number;
     opened: boolean;
     onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
+const generateTravelTimeString = (departureDate: Date, arrivalDate: Date) => {
+    const travelTimeInMinutes =
+        (arrivalDate.getTime() - departureDate.getTime()) / 60000;
+
+    const travelTimeHours = Math.floor(travelTimeInMinutes / 60);
+    const travelTimeMinutes = travelTimeInMinutes - travelTimeHours * 60;
+
+    const travelTimeString = `${travelTimeHours}h${travelTimeMinutes}`;
+
+    return travelTimeString;
+};
+
 const ConnectionInfo: React.FC<Props> = ({
-    departureTime,
-    arrivalTime,
+    departureCity,
+    arrivalCity,
     trainType,
-    travelTimeString,
     price,
     opened,
-    onClick
+    onClick,
 }) => {
+    const departureDate = new Date(departureCity.date);
+    const arrivalDate = new Date(arrivalCity.date);
+
+    const departureTime = `${departureDate.getHours()}:${departureDate.getMinutes()}`;
+    const arrivalTime = `${arrivalDate.getHours()}:${arrivalDate.getMinutes()}`;
+
+    const travelTimeString = generateTravelTimeString(
+        departureDate,
+        arrivalDate
+    );
+
     let compiledClassName = classes.info;
 
-    if(opened) {
+    if (opened) {
         compiledClassName += " " + classes.active;
     }
 
